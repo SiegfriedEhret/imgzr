52bec0667bbf72f2a901104889249c446bd16336
Siegfried Ehret <siegfried.ehret@gmail.com>
2012-01-13 09:50:49 +0100

    added screen orientation detection updated demo

--- imgzr.js
+++ imgzr.js
@@ -16,17 +16,25 @@
 
 (function($){
   var defaults = {
-    resolutions:{
-      small:{min:0, max:480},
-      medium:{min:480, max:768},
-      large:{min:768, max:10000}
-    }
-  }
+      resolutions:{
+        small:{min:0, max:480},
+        medium:{min:480, max:768},
+        large:{min:768, max:10000}
+      }
+    },
+    getScreenWidth = function() {
+      if (window.orientation && (window.orientation == 90 || window.orientation == -90)) {
+        return screen.height;
+      }
+      return screen.width;
+    };
   $.fn.imgzr = function(options) {
-    var settings = $.extend({}, defaults, options);
+    var settings = $.extend({}, defaults, options),
+      screenWidth = getScreenWidth();
+    alert(screenWidth);
     for (var i in settings.resolutions) {
       if(settings.resolutions.hasOwnProperty(i)) {
-        if (settings.resolutions[i].min <= screen.width && screen.width <= settings.resolutions[i].max) {
+        if (settings.resolutions[i].min <= screenWidth && screenWidth <= settings.resolutions[i].max) {
           $(this).find('img').each(function() {
             $(this).attr('src', $(this).data(i));
           });
